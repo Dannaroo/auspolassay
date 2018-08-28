ab9b78c319e1558017c0a84c7a7f1ba759aadf7e
Danny Ward <liege88@gmail.com>
2018-08-28 20:23:37 +0800

    create gh-pages and add references.

--- js/app.js
+++ js/app.js
@@ -6,4 +6,12 @@ $('.col ul').hide();
 $('.main-content').on('click', 'h3', function(event) {
 //show the spoiler text
     $(event.target).siblings().slideToggle();
+// TODO: FiX THIS CODE SO THAT THE SELECTED COLUMN BECOMES THE LAST CHILD.
+    // $(event.target).insertAfter(event.parent().last());
+});
+
+$("h3").hover(function(){
+    $(this).css("color", "gold");
+    }, function(){
+    $(this).css("color", "inherit");
 });
